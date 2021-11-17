32296ea77e4d1178f69d6c62c6c1a6bbf3524d84
Prithvi Punjabi <prithvidais@gmail.com>
2021-11-17 04:48:23 -0500

    Added deleteCommentByID and deleteAllComments

--- data/comments.js
+++ data/comments.js
@@ -5,19 +5,18 @@ const utils = require("../helper/utils");
 const errorCode = require("../helper/common").errorCode;
 const products = require("./products");
 const { ObjectId } = require("mongodb");
-const { comments } = require("../config/mongoCollections");
 
 async function create(product_id, user_id, comment) {
   validator.checkNonNull(product_id),
-  validator.checkNonNull(user_id),
-  validator.checkNonNull(comment);
+    validator.checkNonNull(user_id),
+    validator.checkNonNull(comment);
   validator.checkString(product_id, "Product ID"),
-  validator.checkString(user_id, "User ID"),
-  validator.checkString(comment, "Comment");
+    validator.checkString(user_id, "User ID"),
+    validator.checkString(comment, "Comment");
   validator.isValidObjectID(ObjectId(product_id)),
-  validator.isValidObjectID(ObjectId(user_id));
+    validator.isValidObjectID(ObjectId(user_id));
   const thisproduct = await products.getById(product_id);
-  const currentproduct = await comments.getById(product_id);
+  const currentproduct = await getAllComments(product_id);
   const comments = await comments();
   if (thisproduct) {
     if (currentproduct) {
@@ -31,7 +30,8 @@ async function create(product_id, user_id, comment) {
         { product_id: ObjectId(product_id) },
         { $push: { comments: newComment } }
       );
-      if (insertInfo.insertedCount === 0) throw "Could not add comment";
+      if (insertInfo.modifiedCount === 0) throw "Could not add comment";
+      else return insertInfo.upsertedId;
     } else {
       let newProdComment = {
         product_id: ObjectId(product_id),
@@ -46,14 +46,45 @@ async function create(product_id, user_id, comment) {
       };
       const insertInfo = await comments.insertOne(newProdComment);
       if (insertInfo.insertedCount === 0) throw "Could not add comment";
+      else return insertInfo.insertedId;
     }
   } else throw `Product with ID ${product_id} does not exist`;
 }
 
-async function getAllComments(reviewId){}
+async function getAllComments(reviewId) {}
+
+async function getCommentById(id) {}
+
+async function deleteCommentById(id) {
+  validator.checkNonNull(id),
+    validator.checkString(id),
+    validator.isValidObjectID(id);
+  let thiscomment = getCommentById(id);
+  if (thiscomment) {
+    const comments = await comments();
+    const deletedInfo = await comments.deleteOne({
+      _id: ObjectId(id),
+    });
+    if (deletedInfo.deletedCount === 0) throw "Could not delete comment";
+  } else throw `Comment with ID ${id} does not exist`;
+}
+
+async function deleteAllComments(product_id) {
+  validator.checkNonNull(product_id),
+    validator.checkString(product_id),
+    validator.isValidObjectID(product_id);
+  const comments = await comments();
+  const deletedInfo = await comments.deleteOne({
+    product_id: ObjectId(product_id),
+  });
+  if (deletedInfo.deletedCount === 0) throw "Could not delete all comments";
+  return `All comments for Product with ID ${product_id} deleted successfully`;
+}
 
 module.exports = {
   create,
-  getAllComments
-
+  getAllComments,
+  getCommentById,
+  deleteCommentById,
+  deleteAllComments,
 };
