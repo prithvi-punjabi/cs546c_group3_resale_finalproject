58d5f7dfe1f775473e184797ad52b7592c49927c
Prithvi Punjabi <prithvidais@gmail.com>
2021-11-17 03:27:31 -0500

    Changed inserted IDs to Object IDs

--- data/comments.js
+++ data/comments.js
@@ -8,27 +8,33 @@ const { ObjectId } = require("mongodb");
 const { comments } = require("../config/mongoCollections");
 
 async function create(product_id, user_id, comment) {
+  validator.checkNonNull(product_id),
+    validator.checkNonNull(user_id),
+    validator.checkNonNull(comment);
+  validator.checkString(product_id, "Product ID"),
+    validator.checkString(user_id, "User ID"),
+    validator.checkString(comment, "Comment");
   const thisproduct = await products.getById(product_id);
   // const thisproduct = await comments.getById(product_id);
   const comments = await comments();
   if (thisproduct) {
     let newComment = {
       _id: new ObjectId(),
-      user_id: user_id,
+      user_id: ObjectId(user_id),
       dateAdded: new Date(),
       comment: comment,
     };
     const insertinfo = await comments.updateOne(
-      { product_id: product_id },
+      { product_id: ObjectId(product_id) },
       { $push: { comments: newComment } }
     );
   } else {
     let newProdComment = {
-      product_id: product_id,
+      product_id: ObjectId(product_id),
       comments: [
         {
           _id: new ObjectId(),
-          user_id: user_id,
+          user_id: ObjectId(user_id),
           dateAdded: new Date(),
           comment: comment,
         },
