9b8e31153b302bacb9bf675548631c7f7989d240
Prithvi Punjabi <prithvidais@gmail.com>
2021-12-09 05:51:22 -0500

    Added tooltips for accessibility, css fixes

--- public/js/clientside2.js
+++ public/js/clientside2.js
@@ -270,4 +270,10 @@
       });
     }
   });
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 })(window.jQuery);
